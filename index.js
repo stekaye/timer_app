05384e57ab104ecc4c5be23cc1736209a77d76b9
Stephen Kaye <stephenkayebarcelona@gmail.com>
2020-10-09 09:52:11 +0200

    Initial logic. Create constructor class, event listeners and create start and pause methods

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Hi there!');
+class Timer {
+  constructor (durationInput, startButton, pauseButton) {
+    //This enables other methods to use them.
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    //automatically set up event listener on each new timer instance for whatever is defined as the start button.
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  //Define start method with an arroq function. This will ensure that the value of this is equal to Timer. Otherwise, it would refer to the button. Option two would be to add .bind(this) after this.start above.
+  start = () => {
+    this.tick();
+    //When we call setInterval we get back something called a timer(an ID). We assign it to an instance variable (THIS enables us to share information between different methods). To stop the interval, we later call clearInterval(id);
+    this.interval = setInterval(this.tick, 1000);
+  }
+
+  tick = () => {
+    console.log('tick')
+  }
+
+  pause = () => {
+    clearInterval(this.interval);
+  }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
